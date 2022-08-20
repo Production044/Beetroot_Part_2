b393d0b5a3fa1daa151a5650dc25886f102d0cd2
Production044 <99043038+Production044@users.noreply.github.com>
2022-08-20 11:54:31 +0100

    Delete decorator.py

--- 14_lesson/decorator.py
+++
@@ -1,9 +0,0 @@
-def box_2(function):
-
-    def wrapper(name):
-        result = function(name)
-        print('*' * len(result))
-        print(result)
-        print('*' * len(result))
-
-    return wrapper
